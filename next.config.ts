d1c7cb35f6f267bae040e13e854cc93408f77ae6
anaj00 <jana_bantolino@dlsu.edu.ph>
2026-07-09 18:32:30 +0800

    chore: support root folder

--- next.config.ts
+++ next.config.ts
@@ -56,7 +56,7 @@ const nextConfig: NextConfig = {
     routes.forEach(({ hosts, destination }) => {
       hosts.forEach((host) => {
         rewrites.push({
-          source: "/:path((?!_next|company|university|admin|gcs-proxy|invite).*)*",
+          source: "/:path((?!_next|betterinternship-logo|BetterInternshipLogo|company(?:/|$)|university(?:/|$)|admin(?:/|$)|gcs-proxy(?:/|$)|invite(?:/|$)).*)*",
           has: [{ type: "host", value: host }],
           destination: `/${destination}/:path*`,
         });
